6185179fc20df3fca24781da9c9665fe9c2a6a20
Ali Irfan <irfanserboon562@gmail.com>
2025-12-27 15:12:29 +0500

    feat: created tests for homepage route

--- test/imageController.test.js
+++ test/imageController.test.js
@@ -10,4 +10,18 @@ describe("GET /health", () => {
   });
 });
 
+describe("GET /", () => {
+  describe("GET the main homepage in text/html form", () => {
+    test("should return 200 status code", async () => {
+      const response = await request(app).get("/");
+      expect(response.statusCode).toBe(200);
+    });
+    test("should have return type text/html page", async () => {
+      const response = await request(app).get("/");
+      expect(response.headers["content-type"]).toEqual(
+        expect.stringContaining("text/html")
+      );
+    });
+  });
+});
 
